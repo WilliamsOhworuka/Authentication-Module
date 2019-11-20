dd78de95ab1fd114ea583fca7f1085ed25df01f0
unknown <williamsohworuka@yahoo.com>
2019-11-20 21:45:23 +0100

    setup input validation

---
+++ src/helpers/validator.js
@@ -0,0 +1,51 @@
+import { check } from 'express-validator';
+import userService from '../services/userService';
+
+const { findByEmail } = userService;
+
+const isValidEmail = (field = 'email') => check(field)
+  .trim()
+  .normalizeEmail()
+  .isEmail()
+  .withMessage(`invalid ${field}`)
+  .not()
+  .isEmpty()
+  .withMessage('email is a required field');
+
+const isValidUsername = (field) => check(field)
+  .trim()
+  .custom((value) => {
+    const regex = /^[a-z]*[-]{0,1}[a-z]+$/i;
+    return regex.test(value);
+  }).withMessage(`${field} can only contain alphabets and a dash(-)`)
+  .not()
+  .isEmpty()
+  .withMessage(`${field} is a required field`);
+
+
+const isValidPassword = (field = 'password') => check(field)
+  .isLength({ min: 6 })
+  .withMessage(`${field} must be at least 6 characters long`)
+  .not()
+  .isEmpty()
+  .withMessage(`${field} is a required field`)
+  .isAlphanumeric()
+  .withMessage(`${field} should contain only numbers and alphabets`);
+
+const notDuplicate = (field = 'email') => check(field)
+  .trim()
+  .normalizeEmail()
+  .custom((value) => {
+    findByEmail(value).then((user) => {
+      if (user) {
+        return Promise.reject(new Error(`${field} already exists`));
+      }
+    });
+  });
+
+export default {
+  notDuplicate,
+  isValidEmail,
+  isValidPassword,
+  isValidUsername
+};
